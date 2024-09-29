95868b2872766d246737c523c965d25488b464f2
Mariana Araújo de Souza Monteiro <88739032+Mariana-ASM@users.noreply.github.com>
2024-09-28 21:40:53 -0300

    Update fichadopersonagem.py

--- fichadopersonagem.py
+++ fichadopersonagem.py
@@ -1,1 +1,7 @@
 #ficha do personagem
+"O que é uma classe em POO?": ("(a) Molde ou modelo para criar objetos.", "(b) Função que executa um código automaticamente.", "a"),
+            "O que é um objeto em POO?": ("(a) Um loop de repetição.", "(b) Instância de uma classe.", "b"),
+            "O que é herança em POO?": ("(a) Compartilhamento de atributos e métodos entre classes.", "(b) Atributos privados de uma classe.", "a"),
+            "O que é encapsulamento em POO?": ("(a) Restrição do acesso direto a atributos.", "(b) Criação de uma nova classe.", "a"),
+            "O que é polimorfismo em POO?": ("(a) Permitir que várias classes usem métodos com o mesmo nome.", "(b) Permitir que uma função tenha vários loops.", "a")
+        }
